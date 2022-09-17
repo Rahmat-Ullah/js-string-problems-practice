f0770101c7d66ec6cf732c6716fb025213293d89
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-17 20:39:14 +0600

    string in js practiced

---
+++ case.js
@@ -0,0 +1,12 @@
+const userName = 'blackpink'
+const userInput = 'blackpink'
+
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+console.log(userName.toLocaleLowerCase());
+if(userName.toUpperCase === userInput.toUpperCase){
+    console.log('valid user');
+}
+else{
+    console.log('invalid user');
+}
