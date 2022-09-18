05be6e9d0bfd44a86dc6a6034f287a95208ef70c
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-18 19:29:32 +0600

    js Math abs round ceil floor and random number practiced

---
+++ math.js
@@ -0,0 +1,26 @@
+const result = Math.pow(3, 8);
+// console.log(result);
+
+const num1 = 24;
+const num2 = 44;
+
+const gap = Math.abs(num1 - num2);
+
+if(gap < 5){
+    console.log('You guys can be friend.');
+}
+else{
+    console.log('You guys stay apart');
+}
+
+const number = 2.4568;
+const fullNumber = Math.round(number);
+// console.log(fullNumber);
+
+const result2 = Math.ceil(number);
+const result3 = Math.floor(number);
+// console.log(result2);
+// console.log(result3);
+
+const random = Math.random(number);
+console.log(random);
