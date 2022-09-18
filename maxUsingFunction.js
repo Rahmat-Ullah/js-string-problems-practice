45371048b510e1cfc73556f202c5763a90a1a46f
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-18 22:46:31 +0600

    finding maximum number using js function and if else syntax

---
+++ maxUsingFunction.js
@@ -0,0 +1,15 @@
+function threeNumbers(num1, num2, num3){
+    if(num1 > num2 && num1 > num3){
+        console.log('num1 is the largest number');
+    }
+    else if(num2 > num1 && num2 > num3){
+        console.log('num2 is the largest number');
+    }
+    else{
+        console.log('num3 is the largest number');
+    }
+
+    return num1, num2, num3;
+}
+
+threeNumbers(23, 4, 5);
